f67ecdb71578ec7de50de82bee6c0c7a7c96fd85
AldebaraFork <eduardoamorim758@proton.me>
2024-07-16 13:11:28 -0300

    testing class'

--- FirstActivity/Main.java
+++ FirstActivity/Main.java
@@ -1,13 +1,11 @@
 public class Main{
+
+    //METODO PRINCIPAL
     public static void main(String[] args)
     {
-       int x = 0;
-       int y = 15;
-
-       for (int i = 0; i < y; i++){
-          x++;
-         System.out.println(x);
-       }
-       System.out.println("valor final de x: " + x);
+       
     }
+
+    
 }
+

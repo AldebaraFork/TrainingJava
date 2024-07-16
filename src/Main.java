663ef800a48a7836a7572bbf5f5d39c372eb9dfc
AldebaraFork <eduardoamorim758@proton.me>
2024-07-15 23:17:00 -0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+    public static void main(String[] args) {
+        {
+          int x = 0;
+          int y = 5;
+
+
+          for (int i = 0; x <= 5; x++) {
+
+          }
+
+        }
+    }
+}

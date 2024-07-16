731f66e78a80c4fcd1ff484088e1868b88778cb2
AldebaraFork <eduardoamorim758@proton.me>
2024-07-16 12:57:09 -0300

    First exercise

--- FirstActivity/Main.java
+++ FirstActivity/Main.java
@@ -1,6 +1,13 @@
 public class Main{
     public static void main(String[] args)
     {
-       
+       int x = 0;
+       int y = 15;
+
+       for (int i = 0; i < y; i++){
+          x++;
+         System.out.println(x);
+       }
+       System.out.println("valor final de x: " + x);
     }
 }

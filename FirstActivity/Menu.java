ff4a27000ef857ed020bf9b4c5bafc3c4d2b0c79
AldebaraFork <eduardoamorim758@proton.me>
2024-07-16 18:29:47 -0300

    menu class

---
+++ FirstActivity/Menu.java
@@ -0,0 +1,11 @@
+public class Menu {
+    public static void MenuPrincipal(){
+        System.out.println("Bem vindo ao menu principal! Selecione uma opção abaixo");
+        System.out.println("1 - Cadastrar cliente");
+        System.out.println("2 - Ver clientes cadastrados");
+        System.out.println("3 - Excluir clientes");
+        System.out.println("0 - sair");
+        
+
+    }
+}

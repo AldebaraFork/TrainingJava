0febea61b1933a9fc8f0d2971ea3a29bf4b3554f
AldebaraFork <eduardoamorim758@proton.me>
2024-07-17 14:25:40 -0300

    Add client feature

--- FirstActivity/Main.java
+++ FirstActivity/Main.java
@@ -1,8 +1,12 @@
+
+
 public class Main{
 
     //METODO PRINCIPAL
     public static void main(String[] args)
     {
+       Menu menu = new Menu();
+       menu.MenuPrincipal();
        
     }
 
--- FirstActivity/Menu.java
+++ FirstActivity/Menu.java
@@ -1,11 +1,31 @@
+import java.util.Scanner;
 public class Menu {
-    public static void MenuPrincipal(){
+    
+    public  void MenuPrincipal(){
+        Scanner ler = new Scanner(System.in);
+        DadosPessoa pessoa = new DadosPessoa();
         System.out.println("Bem vindo ao menu principal! Selecione uma opção abaixo");
         System.out.println("1 - Cadastrar cliente");
         System.out.println("2 - Ver clientes cadastrados");
         System.out.println("3 - Excluir clientes");
         System.out.println("0 - sair");
+        short opcao = ler.nextShort();
+        switch (opcao){
+            case 1 :
+                pessoa.CadastrarCliente();                   
+                break;
+            case 2:
+    
+                break;
+            case 3:
+    
+                break;
+            case 0:
+               System.exit(1);
+                break;
+            default:
+                break;
+        }
+ }
         
-
-    }
 }

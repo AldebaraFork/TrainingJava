9fba0227e9f1da13405a457c60e64752b2cf3577
AldebaraFork <eduardoamorim758@proton.me>
2024-07-19 22:24:04 -0300

    starting products class

---
+++ ShopSystem/src/GerenciarProdutos.java
@@ -0,0 +1,11 @@
+public class GerenciarProdutos {
+
+    protected String NomeProduto;
+
+    protected double PrecoProduto;
+
+    protected short QuantidadeProduto;
+
+    protected String Id;
+    
+}

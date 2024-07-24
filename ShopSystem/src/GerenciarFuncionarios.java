8382387bfe3807728ce85eef5e629efc7ba60407
AldebaraFork <eduardoamorim758@proton.me>
2024-07-24 14:16:04 -0300

    employers system working

--- ShopSystem/src/GerenciarFuncionarios.java
+++ ShopSystem/src/GerenciarFuncionarios.java
@@ -1,3 +1,4 @@
+import java.sql.Array;
 import java.sql.Time;
 import java.util.ArrayList;
 import java.util.Random;
@@ -15,24 +16,68 @@ public class GerenciarFuncionarios {
     protected String NomeFuncionario;
     protected Random IdFuncionario = new Random();
     //CARGA HORARIA É DEFINIDA POR INTS NA ORDEM: HORAS, MINUTOS, SEGUNDOS
-    protected Time CargaHoraria = new Time(8);
+    protected Time CargaHoraria;
+    protected double SalarioFuncionario;
+    protected String CargoFuncionario;
+
+
+    //PROPRIEDADES PARA SALVAR O FUNCIONARIO NA LISTA
+    protected String Nome;
     protected double Salario;
+    protected String Cargo;
+    protected Time Horas;
 
 
     //METODO PARA CADASTRO DE FUNCIONARIOS
     public void CadastrarFuncionario(){
 
+        //PROPRIEDADES PODEM MUDAR DE ACORDO COM O CARGO DO FUNCIONARIO
+
+        //VERIFICAÇÃO DE CARGO E OBTER OS DADOS DO FUNCIONARIOq
         try {
 
-            //OBTEM OS DADOS DO USUARIO E CADASTRA NA LISTA
-            System.out.print("\033[H\033[2J");
-            System.out.println("Insira o nome do funcionario: ");
-            NomeFuncionario = ler.nextLine().toUpperCase();
-            //VERIFICA SE O NOME DO FUNCIONARIO ESTA VAZIO
-            if (NomeFuncionario.isEmpty()) {
-                System.out.println("Insira um valor valido");
+
+            System.out.println("Insira o CARGO do funcionar:");
+            System.out.println("Cargos disponiveis: JOVEM APRENDIZ, FUNCIONARIO, SUPERVISOR");
+            CargoFuncionario = ler.nextLine().toUpperCase();
+            //FUNCIONARIO SEJA JOVEM APRENDIZ
+            if (CargoFuncionario.equals("JOVEM APRENDIZ")){
+                Salario = 1400;
+                CargaHoraria = new Time(6);
+            }
+            //FUNCIONARIO PADRAO
+            else if (CargoFuncionario.equals("FUNCIONARIO")){
+                Salario = 2500;
+                CargaHoraria = new Time(8);
+            }
+            //SUPERVISOR
+            else if (CargoFuncionario.equals("SUPERVISOR")){
+                Salario = 3500;
+                CargaHoraria = new Time(8);
+            }else {
+                System.out.println("Insira uma opção valida !");
                 menu.RetornarMenuPrincipal();
             }
+
+          //OBTEM O NOME DO FUNCIONARIO
+            System.out.println("Insira o nome do funcionario");
+            NomeFuncionario = ler.nextLine().toUpperCase();
+
+
+            //INSTANCIA PARA SALVAR O FUNCIONARIO
+
+            GerenciarFuncionarios novoFuncionario = new GerenciarFuncionarios();
+            novoFuncionario.Nome = NomeFuncionario;
+            novoFuncionario.Salario = SalarioFuncionario;
+            novoFuncionario.Cargo = CargoFuncionario;
+            novoFuncionario.Horas = CargaHoraria;
+
+            //SALVA O FUNCIONARIO
+            
+
+
+
+
 
         }catch(Exception ex){
 

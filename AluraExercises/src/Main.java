bb7cd6c937a1e76011ba6d216b95c0ecc9bc8c16
AldebaraFork <eduardoamorim758@proton.me>
2024-07-27 21:22:54 -0300

    Somatoria exercise

--- AluraExercises/src/Main.java
+++ AluraExercises/src/Main.java
@@ -8,55 +8,14 @@ public class Main {
 
 
 
-        //SISTEMA DE FESTA
+        int Numero = 0;
 
-        //PROPRIEDADES
-        String nome;
-        int idade;
-        boolean acompanhado;
+        int NumeroSoma = 10;
 
-
-        //SISTEMA
-        System.out.println("Digite seu nome: ");
-        nome = ler.nextLine().toUpperCase();
-        if (nome.isEmpty()){
-            System.out.println("Insira um valor valido");
+        for (int i = 0; i <= NumeroSoma  ; i++) {
+            Numero += i;
+            System.out.println(Numero);
         }
-
-
-        //VERIFICA A IDADE E ACOMPANHAMENTO
-        System.out.println("Digite sua idade: ");
-        idade = ler.nextInt();
-        System.out.println("Você está acompanhado? ");
-        String verificar = ler.next().toUpperCase();
-
-        //VERIFICA SE O USUARIO DIGITOU SE ESTÁ ACOMPANHADO
-        if (verificar.isEmpty()){
-            System.out.println("Insira um valor valido");
-        }else if (verificar.equals("SIM") || verificar.equals("S")){
-            acompanhado = true;
-
-            //CASO O USUARIO SEJA DE MENOR MAS ESTEJA ACOMPANHADO
-            if (idade < 18 || acompanhado == true){
-                System.out.println("Bem vindo");
-            }
-            //SE FOR DE MAIOR
-            else if (idade >= 18 ){
-                System.out.println("Bem vindo");
-            }
-        }else if (verificar.equals("NAO") || verificar.equals("N")){
-            acompanhado = false;
-            if (idade < 18 || acompanhado){
-                System.out.println("Você não pode entrar, é de menor e não está acompanhado");
-            }else if (idade >= 18 || acompanhado){
-                System.out.println("Bem vindo");
-            }
-        }else {
-            System.out.println("Insira um valor valido");
-        }
-
-
-
 
 
     }

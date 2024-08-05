db6654ed296e4059ba3823139deb1b89696c5322
AldebaraFork <eduardoamorim758@proton.me>
2024-08-05 16:53:27 -0300

    Sorry guys, i was studying get set methods so i can apply better OO

---
+++ HackerRank/src/Cliente.java
@@ -0,0 +1,26 @@
+public class Cliente {
+
+    //PROPRIEDADES DE UM CLIENTE
+    private String nome;
+    private String cpf;
+    private String rg;
+    private int idade;
+    private char sexo;
+
+    //METODOS DE TODO USUARIO
+
+
+    //APLICANDO GET E SET
+    public String getNome() {
+
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    
+
+
+
+}
